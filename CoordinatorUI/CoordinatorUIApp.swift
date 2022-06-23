963ea46bd31224f523eaf149cce12ecde2c47f42
Andrii Kyrychenko <akyrych84@gmail.com>
2022-06-23 18:45:25 -0400

    Initial Commit

---
+++ CoordinatorUI/CoordinatorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorUIApp.swift
+//  CoordinatorUI
+//
+//  Created by Andrii Kyrychenko on 23/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
