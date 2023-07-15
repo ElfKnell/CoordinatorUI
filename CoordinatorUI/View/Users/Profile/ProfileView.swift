b03d1696fdfb1091b70a0aec2c1e34cac5132c9b
Andrii Kyrychenko <akyrych84@gmail.com>
2023-07-15 11:09:12 -0300

    Added ProfileView.

---
+++ CoordinatorUI/View/Users/Profile/ProfileView.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileView.swift
+//  CoordinatorUI
+//
+//  Created by Andrii Kyrychenko on 15/07/2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        List {
+            Section {
+                
+            }
+            
+            Section("General") {
+                
+            }
+            
+            Section("Account") {
+                
+            }
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
